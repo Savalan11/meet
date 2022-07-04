8a56f3878fe1d80eb20ed3529c537739b3cc6766
Savalan Suleymanli <savalan.suleymanli@gmail.com>
2022-07-04 23:22:39 +0200

    Update EndToEnd.test.js

--- src/__tests__/EndToEnd.test.js
+++ src/__tests__/EndToEnd.test.js
@@ -22,19 +22,21 @@ describe("show/hide an event details", () => {
   });
 
   test("An event element is collapsed by default", async () => {
-    const eventDetails = await page.$(".event .event__moreDetails");
+    const eventDetails = await page.$('.event .event__Details');
     expect(eventDetails).toBeNull();
   });
 
   test("User can expand an event to see its details", async () => {
-    await page.click(".event .event__detailsButton");
-    const eventDetails = await page.$(".event .event__moreDetails");
+    await page.click('.event .details-btn');
+    const eventDetails = await page.$('.event .event__Details');
     expect(eventDetails).toBeDefined();
   });
 
   test("User can collapse an event to hide its details", async () => {
-    await page.click(".event .event__detailsButton");
-    const eventDetails = await page.$(".event .event__moreDetails");
+    await page.click('.event .details-btn');
+    const eventDetails = await page.$('.event .event__Details');
     expect(eventDetails).toBeNull();
   });
 });
+
+
