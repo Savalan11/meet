9f3e4711a2822509f392a28aa4ad4cadf9582324
Savalan Suleymanli <savalan.suleymanli@gmail.com>
2022-07-07 12:46:07 +0200

    Update Alert.js

--- src/Alert.js
+++ src/Alert.js
@@ -1,21 +1,29 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 
 class Alert extends Component {
   constructor(props) {
     super(props);
     this.color = null;
+    this.backgroundColor = null;
+    this.borderRadius = null;
+    this.top = null;
   }
 
   getStyle = () => {
     return {
       color: this.color,
+      backgroundColor: this.backgroundColor,
+      borderRadius: this.borderRadius,
+      top: this.top,
     };
   };
 
   render() {
     return (
-      <div className="Alert">
-        <p style={this.getStyle()}>{this.props.text}</p>
+      <div>
+        <p style={this.getStyle()} className="Alert">
+          {this.props.text}
+        </p>
       </div>
     );
   }
@@ -24,15 +32,31 @@ class Alert extends Component {
 class InfoAlert extends Alert {
   constructor(props) {
     super(props);
-    this.color = "blue";
+    this.color = 'rgb(21, 63, 177)';
+    this.backgroundColor = 'rgb(182, 215, 255)';
+    this.borderRadius = '8px';
+    this.top = '205px';
   }
 }
 
 class ErrorAlert extends Alert {
   constructor(props) {
     super(props);
-    this.color = "red";
+    this.color = 'rgb(162, 22, 22)';
+    this.backgroundColor = 'rgb(255, 202, 202)';
+    this.borderRadius = '8px';
+    this.top = '290px';
   }
 }
 
-export { InfoAlert, ErrorAlert };
+class OfflineAlert extends Alert {
+  constructor(props) {
+    super(props);
+    this.color = 'rgb(255, 98, 0)';
+    this.backgroundColor = 'rgb(202,202,202)';
+    this.borderRadius = '8px';
+    this.top = '90px';
+  }
+}
+
+export { InfoAlert, ErrorAlert, OfflineAlert };
